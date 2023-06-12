443649a1f9df55c3eae875e84cc0d1f7c2a71a55
cheorodio <cheorodio@gmail.com>
2023-06-12 16:11:04 +0200

    Add about and highlight sections in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,8 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import connect from '../public/images/icons/connect.png';
+import discover from '../public/images/icons/discover.png';
+import explore from '../public/images/icons/explore.png';
 import styles from './page.module.scss';
 
 export default function Home() {
@@ -10,6 +14,49 @@ export default function Home() {
         <Link href="/" className={styles.heroLink}>
           eplore now
         </Link>
+      </section>
+
+      <section className={styles.aboutSection}>
+        <h2>Who are we?</h2>
+        <p className={styles.introduction}>
+          GoLokal is your ultimate destination for local and handmade products.
+          We are passionate about supporting artisans, craftsmen, and local
+          businesses, and our platform serves as a virtual marketplace to
+          showcase their unique creations.
+        </p>
+        <div className={styles.highlights}>
+          <div>
+            <h6>01</h6>
+            <h4>Explore</h4>
+            <Image className={styles.iconHidden} src={explore} alt="" />
+            <p>
+              Explore a thoughtfully curated collection of local and handmade
+              products, handpicked for their exceptional quality, uniqueness,
+              and craftsmanship.
+            </p>
+          </div>
+          <div>
+            <h6>02</h6>
+            <h4>Discover</h4>
+            <Image className={styles.iconHidden} src={discover} alt="" />
+            <p>
+              Discover the stories and talents of local artisans and craftsmen
+              who pour their passion into creating each handmade item,
+              empowering them to thrive and preserve their traditional skills.
+            </p>
+          </div>
+          <div>
+            <h6>03</h6>
+            <h4>Connect</h4>
+            <Image className={styles.iconHidden} src={connect} alt="" />
+            <p>
+              Connect with a vibrant community of like-minded individuals who
+              share your love for locally made goods. Engage in discussions,
+              attend events, and collaborate with fellow enthusiasts and
+              artisans.
+            </p>
+          </div>
+        </div>
       </section>
     </main>
   );
