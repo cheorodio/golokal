51567c88b3dbbe65c2f13eafa68909d0bead5903
cheorodio <cheorodio@gmail.com>
2023-06-12 17:34:51 +0200

    Add more sections to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import discoverHeader from '../public/images/discoverHeader.jpg';
 import connect from '../public/images/icons/connect.png';
 import discover from '../public/images/icons/discover.png';
 import explore from '../public/images/icons/explore.png';
+import statistic from '../public/images/statistic.jpg';
 import styles from './page.module.scss';
 
 export default function Home() {
@@ -28,7 +30,11 @@ export default function Home() {
           <div>
             <h6>01</h6>
             <h4>Explore</h4>
-            <Image className={styles.iconHidden} src={explore} alt="" />
+            <Image
+              className={styles.iconHidden}
+              src={explore}
+              alt="explore icon"
+            />
             <p>
               Explore a thoughtfully curated collection of local and handmade
               products, handpicked for their exceptional quality, uniqueness,
@@ -38,7 +44,11 @@ export default function Home() {
           <div>
             <h6>02</h6>
             <h4>Discover</h4>
-            <Image className={styles.iconHidden} src={discover} alt="" />
+            <Image
+              className={styles.iconHidden}
+              src={discover}
+              alt="discover icon"
+            />
             <p>
               Discover the stories and talents of local artisans and craftsmen
               who pour their passion into creating each handmade item,
@@ -48,13 +58,64 @@ export default function Home() {
           <div>
             <h6>03</h6>
             <h4>Connect</h4>
-            <Image className={styles.iconHidden} src={connect} alt="" />
+            <Image
+              className={styles.iconHidden}
+              src={connect}
+              alt="connect icon"
+            />
             <p>
               Connect with a vibrant community of like-minded individuals who
               share your love for locally made goods. Engage in discussions,
               attend events, and collaborate with fellow enthusiasts and
               artisans.
             </p>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.discoverSection}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.discoverImage}
+            src={discoverHeader}
+            alt="Image of a girl wearing handmade polymer clay earrings"
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h2>Discover local vendors with over 1000 handmade products</h2>
+          <p>
+            By supporting local and handmade products, you become an integral
+            part of a movement that promotes community empowerment, fosters
+            creativity, and fuels economic growth at a grassroots level.
+          </p>
+          <Link href="/" className={styles.discoverButton}>
+            explore now
+          </Link>
+        </div>
+      </section>
+
+      <section className={styles.statistics}>
+        <Image
+          className={styles.potteryImage}
+          src={statistic}
+          alt="image of a hand making pottery"
+        />
+        <div className={styles.information}>
+          <div>
+            <h3>1000</h3>
+            <p>handmade products</p>
+          </div>
+          <div>
+            <h3>20</h3>
+            <p>categories to choose from</p>
+          </div>
+          <div>
+            <h3>15</h3>
+            <p>registered vendors</p>
+          </div>
+          <div>
+            <h3>300</h3>
+            <p>registered users</p>
           </div>
         </div>
       </section>
