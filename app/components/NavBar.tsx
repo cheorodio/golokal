4a344c57c37524291c5e7e8ff23a98bd345966ca
cheorodio <cheorodio@gmail.com>
2023-06-12 14:58:31 +0200

    Add mobile navigation

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,13 +1,21 @@
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { CgClose } from 'react-icons/cg';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import styles from './NavFooter.module.scss';
 
 export default function NavBar() {
+  const [navbarOpen, setNavbarOpen] = useState(true);
+
   return (
     <header className={styles.navigationBar}>
       <div className={styles.left}>
         <Link href="/#" className={styles.logo}>
           GoLokal
         </Link>
+
         <nav>
           <ul className={styles.navLinks}>
             <Link href="/">
@@ -25,12 +33,24 @@ export default function NavBar() {
           </ul>
         </nav>
       </div>
+
       <div className={styles.right}>
         <input className={styles.searchBar} placeholder="Search..." />
         <Link href="/" className={styles.loginButton}>
           login
         </Link>
       </div>
+
+      <button
+        className={styles.menuButton}
+        onClick={() => setNavbarOpen((prev) => !prev)}
+      >
+        {navbarOpen ? (
+          <RxHamburgerMenu className={styles.hamburgerIcon} />
+        ) : (
+          <CgClose className={styles.closeIcon} />
+        )}
+      </button>
     </header>
   );
 }
