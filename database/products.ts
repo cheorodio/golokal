b1fa7a2ba44f18706f1800b6b715af17198e0cd0
cheorodio <cheorodio@gmail.com>
2023-06-13 21:06:18 +0200

    Create mock database

---
+++ database/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    id: 1,
+    name: 'soap',
+    alt: 'An image of some handmade soap',
+    company_name: 'the_soap_company',
+    category: 'soap',
+    description:
+      'This is a description about the product. This isa description about the product. This is a description about the product. This isa description about the product.',
+  },
+  {
+    id: 2,
+    name: 'jewellery',
+    alt: 'An image of some handmade jewellery',
+    company_name: 'vida',
+    category: 'jewellery',
+    description:
+      'This is a description about the product. This isa description about the product. This is a description about the product. This isa description about the product.',
+  },
+  {
+    id: 3,
+    name: 'ceramics',
+    alt: 'An image of some handmade cups',
+    company_name: 'lumaShop',
+    category: 'ceramics',
+    description:
+      'This is a description about the product. This isa description about the product. This is a description about the product. This isa description about the product.',
+  },
+];
