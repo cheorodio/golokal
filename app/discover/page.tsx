39589bede41f44f82d965b78c3c8c6020a4b9d21
cheorodio <cheorodio@gmail.com>
2023-06-13 21:09:43 +0200

    Create dynamic page

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,3 +1,32 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { products } from '../../database/products';
+import styles from '../styles/discoverPage.module.scss';
+
 export default function DiscoverPage() {
-  return <div></div>;
+  return (
+    <main className={styles.discoverSection}>
+      <h1>Products</h1>
+      <div className={styles.productsContainer}>
+        {products.map((product) => {
+          return (
+            <div
+              key={`product-div-${product.id}`}
+              className={styles.productContainer}
+            >
+              <Link href="/">{product.company_name}</Link>
+              <Link href="/">
+                <Image
+                  src={`/images/products/${product.name}.jpg`}
+                  alt={product.alt}
+                  width={200}
+                  height={200}
+                />
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
 }
