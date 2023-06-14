006fd950b7846220fdc235fe7e15f3af23517158
cheorodio <cheorodio@gmail.com>
2023-06-14 16:28:09 +0200

    Add mock database for vendors

---
+++ database/vendors.ts
@@ -0,0 +1,27 @@
+export const vendors = [
+  {
+    id: 1,
+    name: 'Lisa',
+    shopName: 'vidaShop',
+    bio: 'This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. ',
+    alt: 'akjhdjka',
+  },
+  {
+    id: 2,
+    name: 'Tony',
+    shopName: 'the_soap_company',
+    bio: 'This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. ',
+  },
+  {
+    id: 3,
+    name: 'Liz',
+    shopName: 'lumaShop',
+    bio: 'This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. ',
+  },
+  {
+    id: 4,
+    name: 'Kate',
+    shopName: 'company_name',
+    bio: 'This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. This is a quick intro about Lisa and products that she makes. ',
+  },
+];
