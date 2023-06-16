49646ad572b767c57ac16026be596790ba2c9286
cheorodio <cheorodio@gmail.com>
2023-06-16 13:34:31 +0200

    Add 512x512 maskable icon for PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#FFF" />
+      </head>
       <body className={inter.className}>
         <NavBar />
         {children}
