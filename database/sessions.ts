a073f02f8e81b9a8c9a83bfce8e1f217274aa2be
cheorodio <cheorodio@gmail.com>
2023-06-18 12:56:55 +0200

    Create session removal after expiry

--- database/sessions.ts
+++ database/sessions.ts
@@ -2,6 +2,15 @@ import { cache } from 'react';
 import { Session } from '../migrations/1687078261-createSessions';
 import { sql } from './connect';
 
+export const deleteExpiredSessions = cache(async () => {
+  await sql`
+    DELETE FROM
+       sessions
+    WHERE
+      expiry_timestamp < now()
+  `;
+});
+
 export const creaateSession = cache(async (token: string, userId: number) => {
   const [session] = await sql<Session[]>`
     INSERT INTO sessions
@@ -13,6 +22,9 @@ export const creaateSession = cache(async (token: string, userId: number) => {
       token,
       user_id
     `;
+
+  // delete all expired sessions
+  await deleteExpiredSessions();
 
   return session;
 });
