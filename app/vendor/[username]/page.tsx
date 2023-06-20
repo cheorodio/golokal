bc3cc4a1485d249daeab692c5e51710b4b26e409
cheorodio <cheorodio@gmail.com>
2023-06-20 17:35:13 +0200

    Change directory name from discover to vendor

---
+++ app/vendor/[username]/page.tsx
@@ -0,0 +1,53 @@
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+import { AiOutlineCamera } from 'react-icons/ai';
+import { getProducts } from '../../../database/products';
+import { getVendorByUsername } from '../../../database/vendors';
+import AddProducts from '../../admin/[username]/AddProducts';
+import styles from '../../styles/shopPage.module.scss';
+
+type Props = {
+  params: { username: string };
+};
+
+export default async function UserProfilePage({ params }: Props) {
+  const vendor = await getVendorByUsername(params.username);
+
+  if (!vendor) {
+    notFound();
+  }
+  const products = await getProducts();
+
+  return (
+    <main>
+      <div className={styles.shopPage}>
+        <div className={styles.shopInfo}>
+          <div className={styles.imageBox}>
+            <AiOutlineCamera />
+          </div>
+          <div className={styles.moreInfo}>
+            <h1>{vendor.shopname}</h1>
+            <div>
+              <Link href="/">www.shopname.com</Link>
+              <button className={styles.followButton}>follow</button>
+            </div>
+            <p className={styles.shopBio}>
+              Information about this shop and their products. Information about
+              this shop and their products. Information about this shop and
+              their products. Information about this shop and their products.
+              Information about this shop and their products. Information about
+              this shop and their products. Information about this shop and
+              their products. Information about this shop and their products.
+            </p>
+          </div>
+        </div>
+        <div className={styles.productsFeed}>
+          <h2>Products Feed</h2>
+          <div className={styles.productsContainer}>
+            <AddProducts products={products} />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
