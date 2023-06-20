097af28f2d7f6b03612d6506855ba2ae91cb7068
cheorodio <cheorodio@gmail.com>
2023-06-20 12:54:12 +0200

    Add and style vendor bio in vendor card in the discover/vendors page

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { AiOutlineCamera } from 'react-icons/ai';
 import { getVendors } from '../../database/vendors';
 import styles from '../styles/vendorsPage.module.scss';
 
@@ -14,9 +15,21 @@ export default async function DiscoverPage() {
               key={`vendor-div-${vendor.id}`}
               className={styles.vendorContainer}
             >
+              <div className={styles.imageBox}>
+                <AiOutlineCamera />
+              </div>
               <div>
                 <p className={styles.vendorName}>{vendor.username}</p>
                 <p className={styles.vendorShop}>{vendor.shopname}</p>
+              </div>
+              <div className={styles.vendorInfo}>
+                <p>
+                  This is a brief intro about this vendor and products that they
+                  sell. Click the link below to visit their profile. This is
+                  also hardcoded, don't be fooled. I'm still working on how to
+                  insert bio into the database without having to do it in the
+                  registration process 😁😁😁😁😁😁😁😁😁😁😁😁😁
+                </p>
               </div>
               <Link
                 href={`/discover/${vendor.username}`}
