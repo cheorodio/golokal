cd6e119a85e25e9eb383049fba0cab1d965c9573
cheorodio <cheorodio@gmail.com>
2023-06-20 12:01:46 +0200

    Display all vendors from database

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -1,41 +1,32 @@
-// import Image from 'next/image';
-// import Link from 'next/link';
-// import { getVendors } from '../../database/vendors';
-import styles from './discoverPage.module.scss';
+import Link from 'next/link';
+import { getVendors } from '../../database/vendors';
+import styles from '../styles/vendorsPage.module.scss';
 
 export default async function DiscoverPage() {
-  // const vendors = await getVendors();
+  const vendors = await getVendors();
   return (
     <main className={styles.discoverSection}>
       <h1>Discover local vendors</h1>
       <div className={styles.productsContainer}>
-        {/* {vendors.map((vendor) => {
+        {vendors.map((vendor) => {
           return (
             <div
               key={`vendor-div-${vendor.id}`}
               className={styles.vendorContainer}
             >
-              <Image
-                className={styles.vendorImage}
-                src={`/images/vendors/${vendor.name}.jpg`}
-                alt=""
-                width={280}
-                height={300}
-              />
               <div>
-                <p className={styles.vendorName}>{vendor.name}</p>
-                <p className={styles.vendorShop}>{vendor.shopName}</p>
+                <p className={styles.vendorName}>{vendor.username}</p>
+                <p className={styles.vendorShop}>{vendor.shopname}</p>
               </div>
-              <p className={styles.vendorBio}>{vendor.bio}</p>
               <Link
-                href={`/discover/${vendor.name}`}
+                href={`/discover/${vendor.username}`}
                 className={styles.visitVendorLink}
               >
                 visit profile
               </Link>
             </div>
           );
-        })} */}
+        })}
       </div>
     </main>
   );
