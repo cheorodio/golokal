5a567616dd8f0064b2c6a7841f7a617cc90e2edc
cheorodio <cheorodio@gmail.com>
2023-06-21 16:15:37 +0200

    Modify user database

--- database/users.ts
+++ database/users.ts
@@ -1,8 +1,8 @@
 import { cache } from 'react';
-import { User } from '../migrations/1686845726-createUsers';
+import { User } from '../migrations/1687352892-createTableUsers';
 import { sql } from './connect';
 
-export type UserWithPasswordHash = User & {
+type UserWithPasswordHash = User & {
   passwordHash: string;
 };
 
@@ -12,40 +12,52 @@ type CreateUser = {
   email: string;
 };
 
+type UserSession = {
+  id: number;
+  username: string;
+};
+
 export const getUserWithPasswordHashByUsername = cache(
   async (username: string) => {
     const [user] = await sql<UserWithPasswordHash[]>`
-SELECT * FROM
-  users
-WHERE
-  users.username = ${username}`;
+    SELECT * FROM
+      users
+    WHERE
+      users.username = ${username.toLowerCase()}
+ `;
+
     return user;
   },
 );
 
 export const getUserByUsername = cache(async (username: string) => {
   const [user] = await sql<User[]>`
-SELECT
-  id,
-  username,
-  email,
-  first_name,
-  bio
-FROM
-  users
-WHERE
-  users.username = ${username.toLowerCase()}`;
+    SELECT
+      id,
+      username,
+      email,
+      profile_name,
+      bio,
+      shop_id,
+      profile_image_id
+    FROM
+      users
+    WHERE
+      users.username = ${username.toLowerCase()}
+ `;
+
   return user;
 });
 
 // creating new users
 export const createUser = cache(
   async (username: string, email: string, passwordHash: string) => {
+    console.log(passwordHash);
     const [user] = await sql<CreateUser[]>`
     INSERT INTO users
-      ( username, email, password_hash)
+      (username, email, password_hash)
     VALUES
-      (${username.toLowerCase()}, ${email}, ${passwordHash})
+      (${username}, ${email}, ${passwordHash})
     RETURNING
       id,
       username,
@@ -56,11 +68,10 @@ export const createUser = cache(
 );
 
 export const getUserBySessionToken = cache(async (token: string) => {
-  const [user] = await sql<CreateUser[]>`
+  const [user] = await sql<UserSession[]>`
   SELECT
     users.id,
-    users.username,
-    users.email
+    users.username
   FROM
     users
   INNER JOIN
