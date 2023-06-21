0c72037465c5b324f58edc093e616341fc4f2b27
cheorodio <cheorodio@gmail.com>
2023-06-21 09:49:12 +0200

    Create 1 session to creating session for both users and vendors

--- database/sessions.ts
+++ database/sessions.ts
@@ -11,37 +11,64 @@ export const deleteExpiredSessions = cache(async () => {
   `;
 });
 
-export const createSession = cache(async (token: string, userId: number) => {
-  const [session] = await sql<Session[]>`
-    INSERT INTO sessions
-      (token, user_id)
-    VALUES
-      (${token}, ${userId})
-    RETURNING
-      id,
-      token
-    `;
-
-  // delete all expired sessions
-  await deleteExpiredSessions();
-  return session;
-});
-
-export const createVendorSession = cache(
-  async (token: string, vendorId: number) => {
+// USERS SESSION ////////////////////////////
+export const createSession = cache(
+  async (token: string, userId: number, vendorId: number) => {
     const [session] = await sql<Session[]>`
     INSERT INTO sessions
-      (token, vendor_id)
+      (token, user_id, vendor_id)
     VALUES
-      (${token}, ${vendorId})
+      (${token}, ${userId}, ${vendorId})
     RETURNING
       id,
-      token
+      token,
+      user_id,
+      vendor_id
     `;
+
+    // delete all expired sessions
     await deleteExpiredSessions();
     return session;
   },
 );
+
+// VENDORS SESSIONS /////////////////////////
+// export const createVendorSession = cache(
+//   async (token: string, vendorId: number) => {
+//     const [vendorSession] = await sql<Session[]>`
+//     INSERT INTO sessions
+//       (token, vendor_id)
+//     VALUES
+//       (${token}, ${vendorId})
+//     RETURNING
+//       id,
+//       token
+//     `;
+//     await deleteExpiredSessions();
+//     return vendorSession;
+//   },
+// );
+
+// get valid session only if a vendor
+// export const getValidSessionByVendorToken = cache(async (token: string) => {
+//   // Get the session if match the token AND is not expired
+//   const [vendorSession] = await sql<
+//     { id: number; token: string; vendorId: number }[]
+//   >`
+//     SELECT
+//       sessions.id,
+//       sessions.token,
+//       session.vendor_id,
+//     FROM
+//       sessions
+//     WHERE
+//       sessions.token = ${token}
+//     AND
+//       sessions.expiry_timestamp > now()
+//   `;
+
+//   return vendorSession;
+// });
 
 export const deleteSessionByToken = cache(async (token: string) => {
   const [session] = await sql<{ id: number; token: string }[]>`
