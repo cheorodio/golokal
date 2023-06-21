c2b161bc374350e9aeed2f52e9caa5c9f93fa381
cheorodio <cheorodio@gmail.com>
2023-06-21 16:16:33 +0200

    Modify columns

--- migrations/1687352892-createTableUsers.ts
+++ migrations/1687352892-createTableUsers.ts
@@ -4,18 +4,17 @@ export type User = {
   id: number;
   username: string;
   email: string;
-  passwordHash: string;
-  profileName: string;
-  bio: string;
-  shopId: string;
-  profileImageId: string;
+  profileName: string | null;
+  bio: string | null;
+  shopId: number | null;
+  profileImageId: number | null;
 };
 
 export async function up(sql: Sql) {
   await sql`
     CREATE TABLE users (
       id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-      username varchar(30) NOT NULL,
+      username varchar(30) NOT NULL UNIQUE,
       email varchar(30) NOT NULL,
       password_hash varchar(30) NOT NULL,
       profile_name varchar(40),
