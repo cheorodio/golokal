46461ba7bcdd0bd2a828ae4278964023e26d20fa
cheorodio <cheorodio@gmail.com>
2023-06-21 09:48:25 +0200

    Add function to verify a vendor

--- database/vendors.ts
+++ database/vendors.ts
@@ -1,9 +1,17 @@
 import { cache } from 'react';
-import { Vendor } from '../migrations/1687100213-createVendors';
+// import { Vendor } from '../migrations/1687100213-createVendors';
 import { sql } from './connect';
 
-export type VendorWithPasswordHash = Vendor & {
+type VendorWithPasswordHash = {
+  id: number;
+  username: string;
+  shopname: string;
+  email: string;
   passwordHash: string;
+  firstName: string | null;
+  websiteLink: string | null;
+  bio: string | null;
+  image: string | null;
 };
 
 type CreateVendor = {
@@ -11,6 +19,14 @@ type CreateVendor = {
   username: string;
   shopname: string;
   email: string;
+};
+
+type VendorProfile = CreateVendor & {
+  id: number;
+  firstName: string | null;
+  websiteLink: string | null;
+  bio: string | null;
+  image: string | null;
 };
 
 export const getVendors = cache(async () => {
@@ -22,39 +38,50 @@ export const getVendors = cache(async () => {
  `;
   return vendors;
 });
-
-export const getVendorWithPasswordHashByUsername = cache(
-  async (username: string) => {
-    const [vendor] = await sql<VendorWithPasswordHash[]>`
-SELECT * FROM
+// ///////////////////////////////////////////////
+// editting vendor profile
+export const getVendorById = cache(async (id: number) => {
+  const [vendor] = await sql<VendorProfile[]>`
+SELECT
+  id,
+  first_name,
+  username,
+  shopname,
+  email,
+  website_link,
+  bio,
+  image
+FROM
   vendors
 WHERE
-  vendors.username = ${username.toLowerCase()}
-`;
-    return vendor;
-  },
-);
+  vendors.id = ${id}`;
+  return vendor;
+});
 
+// ///////////////////////////////////////////////
 // dynamic routing to individual vendor page
-// export const getVendorByShopname = cache(async (shopname: string) => {
-//   const [vendor] = await sql<Vendor[]>`
-// SELECT
-//   id,
-//   first_name,
-//   username,
-//   shopname,
-//   email,
-//   website_link,
-//   bio
-// FROM
-//   vendors
-// WHERE
-//   vendors.shopname = ${shopname.toLowerCase().replaceAll(' ', '')}`;
-//   return vendor;
-// });
-
-// Registering a vendor account
 export const getVendorByUsername = cache(async (username: string) => {
+  const [vendor] = await sql<VendorProfile[]>`
+SELECT
+  id,
+  first_name,
+  username,
+  shopname,
+  email,
+  website_link,
+  bio,
+  image
+FROM
+  vendors
+WHERE
+  vendors.username = ${username}`;
+  return vendor;
+});
+
+// REGISTERING A NEW VENDOR //////////////////////////////////////////////////////////
+// 1. verify if the username is already taken when registering
+// imported in vendorRegister route
+export const verifyVendorByUsername = cache(async (username: string) => {
   const [vendor] = await sql<CreateVendor[]>`
 SELECT
   id,
@@ -68,28 +95,7 @@ WHERE
   return vendor;
 });
 
-export const getVendorsWithLimitAndOffsetBySessionToken = cache(
-  async (limit: number, offset: number, token: string) => {
-    const vendors = await sql<VendorWithPasswordHash[]>`
-      SELECT
-        vendors.*
-      FROM
-        vendors
-      INNER JOIN
-        sessions ON (
-          sessions.token = ${token} AND
-          sessions.expiry_timestamp > now()
-          -- sessions.vendor_id = products.vendor_id
-        )
-      LIMIT ${limit}
-      OFFSET ${offset}
-    `;
-
-    return vendors;
-  },
-);
-
-// creating new vendors
+// 2. when verified that the username is not already taken, proceed to creating a new vendor
 export const createVendor = cache(
   async (
     username: string,
@@ -112,6 +118,44 @@ export const createVendor = cache(
   },
 );
 
+// VENDOR LOGIN //////////////////////////////////////////////////////////
+// verifying the vendor credentials when logging in
+export const getVendorWithPasswordHashByUsername = cache(
+  async (username: string) => {
+    const [vendor] = await sql<VendorWithPasswordHash[]>`
+SELECT * FROM
+  vendors
+WHERE
+  vendors.username = ${username.toLowerCase()}
+`;
+    return vendor;
+  },
+);
+
+// //////////////////////////////////////////////////////////
+// get all the vendor only if a valid session token is passed
+export const getVendorsWithLimitAndOffsetBySessionToken = cache(
+  async (limit: number, offset: number, token: string) => {
+    const vendors = await sql<VendorWithPasswordHash[]>`
+      SELECT
+        vendors.*
+      FROM
+        vendors
+      INNER JOIN
+        sessions ON (
+          sessions.token = ${token} AND
+          sessions.expiry_timestamp > now()
+          -- sessions.vendor_id = products.vendor_id
+        )
+      LIMIT ${limit}
+      OFFSET ${offset}
+    `;
+
+    return vendors;
+  },
+);
+
+// //////////////////////////////////////////////////////////
 export const getVendorBySessionToken = cache(async (token: string) => {
   const [vendor] = await sql<CreateVendor[]>`
   SELECT
@@ -131,3 +175,34 @@ export const getVendorBySessionToken = cache(async (token: string) => {
 
   return vendor;
 });
+
+// //////////////////////////////////////////////////////////
+// updating vendor profile page
+export const updateVendorById = cache(
+  async (
+    id: number,
+    username: string,
+    shopname: string,
+    email: string,
+    firstName: string,
+    websiteLink: string,
+    bio: string,
+    image: string,
+  ) => {
+    const [vendor] = await sql<VendorWithPasswordHash[]>`
+      UPDATE vendors
+      SET
+        username = ${username},
+        shopname = ${shopname},
+        email = ${email},
+        first_name = ${firstName || null},
+        website_link = ${websiteLink || null},
+        bio = ${bio || null},
+        image = ${image || null}
+      WHERE
+        id = ${id}
+        RETURNING *
+    `;
+    return vendor;
+  },
+);
