5e59af96d82b41d56c73d40269588d3bfc34eb15
cheorodio <cheorodio@gmail.com>
2023-06-22 17:21:55 +0200

    Add links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,69 @@
 import Link from 'next/link';
 import styles from '../styles/navFooter.module.scss';
 
+const links = [
+  { id: 1, title: 'Home', link: '/' },
+  { id: 2, title: 'Shops', link: '/shops' },
+  { id: 3, title: 'About', link: '/about' },
+  { id: 4, title: 'Blog', link: '/blog' },
+];
+
+const shops = [
+  { id: 1, title: 'Create a shop', link: '/shops/create-shop' },
+  { id: 2, title: 'Policy', link: '/' },
+];
+
+const support = [
+  { id: 1, title: 'Contact Us', link: '/shops/create-shop' },
+  { id: 2, title: 'FAQ', link: '/' },
+  { id: 3, title: 'Careers', link: '/' },
+  { id: 4, title: 'About Us', link: '/' },
+];
+
 export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <Link href="/#" className={styles.logo}>
-        GoLokal
-      </Link>
-      <div>
-        <Link href="/">About</Link>
-        <Link href="/">FAQ</Link>
+    <footer className={styles.footer}>
+      <div className={styles.logoContainer}>
+        <Link href="/" className={styles.logo}>
+          GoLokal
+        </Link>
       </div>
-    </div>
+      <div className={styles.linksContainer}>
+        <div>
+          <h3>Navigation</h3>
+          <ul className={styles.footerLinks}>
+            {links.map(({ id, title, link }) => (
+              <Link href={link} key={`key-${id}`}>
+                <li>{title}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.footerLinks}>
+          <h3>For vendors</h3>
+          <ul className={styles.footerLinks}>
+            {shops.map(({ id, title, link }) => (
+              <Link href={link} key={`key-${id}`}>
+                <li>{title}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.footerLinks}>
+          <h3>Company</h3>
+          <ul className={styles.footerLinks}>
+            {support.map(({ id, title, link }) => (
+              <Link href={link} key={`key-${id}`}>
+                <li>{title}</li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {/* <div>
+        <Link href="/about">About</Link>
+        <Link href="/">FAQ</Link>
+      </div> */}
+    </footer>
   );
 }
