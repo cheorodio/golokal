0b3616c32654a8f8053c731cc7bcd7828e262af8
cheorodio <cheorodio@gmail.com>
2023-06-22 19:14:52 +0200

    Add function to edit and delete users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,12 @@
 import { cookies } from 'next/headers';
 import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
 import { getValidSessionByToken } from '../../../database/sessions';
-import { getUsersWithLimitAndOffsetBySessionToken } from '../../../database/users';
+import {
+  deleteUserById,
+  getUsersWithLimitAndOffsetBySessionToken,
+  updateUserById,
+} from '../../../database/users';
 import { User } from '../../../migrations/1687352892-createTableUsers';
 
 export type Error = {
@@ -9,6 +14,17 @@ export type Error = {
 };
 
 type UsersResponseBodyGet = { user: User[] } | Error;
+type UserResponseBodyDelete = { user: User[] } | Error;
+type UserResponseBodyPut = { user: User[] } | Error;
+
+const userSchema = z.object({
+  username: z.string(),
+  email: z.string(),
+  profileName: z.string(),
+  bio: z.string(),
+  shopId: z.number(),
+  profileImageId: z.number(),
+});
 
 export async function GET(
   request: NextRequest,
@@ -52,4 +68,88 @@ export async function GET(
   );
 
   return NextResponse.json({ users: users });
+}
+
+// ////////////////////////////////////////////
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Record<string, string | string[]> },
+): Promise<NextResponse<UserResponseBodyDelete>> {
+  const userId = Number(params.userId);
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: 'Invalid User Id',
+      },
+      { status: 400 },
+    );
+  }
+  // query the database to get all the user
+  const user = await deleteUserById(userId);
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        error: 'User Not Found',
+      },
+      { status: 404 },
+    );
+  }
+
+  return NextResponse.json({ user: user });
+}
+
+// ////////////////////////////////////////////
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Record<string, string | string[]> },
+): Promise<NextResponse<UserResponseBodyPut>> {
+  const userId = Number(params.userId);
+  const body = await request.json();
+
+  if (!userId) {
+    return NextResponse.json(
+      {
+        error: 'Invalid User Id',
+      },
+      { status: 400 },
+    );
+  }
+
+  // zod please verify the body matches my schema
+  const result = userSchema.safeParse(body);
+
+  if (!result.success) {
+    // zod sends details about the error
+    return NextResponse.json(
+      {
+        error: 'The data is incomplete',
+      },
+      { status: 400 },
+    );
+  }
+  // query the database to update the user
+  const user = await updateUserById(
+    userId,
+    result.data.username,
+    result.data.email,
+    result.data.profileName,
+    result.data.bio,
+    result.data.shopId,
+    result.data.profileImageId,
+  );
+
+  if (!user) {
+    return NextResponse.json(
+      {
+        error: 'User Not Found',
+      },
+      { status: 404 },
+    );
+  }
+
+  return NextResponse.json({
+    user: user,
+  });
 }
