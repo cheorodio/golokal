e58f1015ebef4995dff66ed6262a50494064a612
cheorodio <cheorodio@gmail.com>
2023-06-23 09:49:30 +0200

    Create products table

---
+++ migrations/1687505841-createTableProducts.ts
@@ -0,0 +1,27 @@
+import { Sql } from 'postgres';
+
+export type Product = {
+  id: number;
+  name: string;
+  categoryId: number;
+  description: string;
+  productImageId: number;
+};
+
+export async function up(sql: Sql) {
+  await sql`
+    CREATE TABLE products (
+      id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+      name varchar(30) NOT NULL,
+      category_id integer NOT NULL,
+      description varchar(500) NOT NULL,
+      product_image_id integer NOT NULL
+    )
+  `;
+}
+
+export async function down(sql: Sql) {
+  await sql`
+    DROP TABLE products
+  `;
+}
