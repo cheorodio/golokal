c87441d4e206b7f58436016b6a5e4742161a3e9d
cheorodio <cheorodio@gmail.com>
2023-06-28 21:02:30 +0200

    Style favourite section in profile

--- app/[username]/MyFavourites.tsx
+++ app/[username]/MyFavourites.tsx
@@ -1,15 +1,38 @@
 'use client';
 
-// import styles from '../styles/EditProfile.module.scss';
+import Image from 'next/image';
+import Link from 'next/link';
+import { VscLocation } from 'react-icons/vsc';
+import styles from '../styles/EditProfile.module.scss';
 
 export default function MyFavourites(props: any) {
   return (
     <>
       {props.favourites.map((favourite: any) => {
         return (
-          <div key={`favourite-div-${favourite.shopId}`}>
-            <p>{favourite.shopName}</p>
-            hi
+          <div
+            key={`favourite-div-${favourite.shopId}`}
+            className={styles.favouriteShopCard}
+          >
+            <Link
+              href={`/shops/${favourite.shopId}`}
+              className={styles.imageSection}
+            >
+              <Image src={favourite.shopImage} alt="Shop image" />
+            </Link>
+            <div className={styles.infoSection}>
+              <div>
+                <h1>{favourite.shopName}</h1>
+                <Link href="/">
+                  <h4>{favourite.shopWebsiteUrl}</h4>
+                </Link>
+              </div>
+              <p className={styles.bio}>{favourite.shopDescription}</p>
+              <p className={styles.location}>
+                <VscLocation />
+                <span>{favourite.shopLocation}</span>
+              </p>
+            </div>
           </div>
         );
       })}
