8b47451015fa9fb8e1b20bd29bacf2554c294c2f
cheorodio <cheorodio@gmail.com>
2023-06-29 16:02:16 +0200

    Add more example shops to database

--- migrations/1687947541-insertShops.ts
+++ migrations/1687947541-insertShops.ts
@@ -9,7 +9,7 @@ export const shops = [
       'Welcome to Vida, where passion and artistry come together to create exquisite handmade jewelry. At Vida, we believe that jewelry is more than just an accessory—it is an expression of individuality, a reflection of style, and a symbol of cherished moments.',
     websiteUrl: 'www.vidajewellery.com',
     location: 'Vienna',
-    imageUrl: '/images/shop.png',
+    imageUrl: '/images/vida.jpg',
   },
   {
     id: 2,
@@ -19,7 +19,27 @@ export const shops = [
       'Welcome to luluShop, your haven for exquisite handmade candles! Nestled in the heart of our community, we take great pride in offering you a diverse collection of artisanal candles that will illuminate your world with warmth and beauty.',
     websiteUrl: 'www.luluShop.com',
     location: 'Vienna',
-    imageUrl: '/images/shop.png',
+    imageUrl: '/images/lulushop.jpg',
+  },
+  {
+    id: 3,
+    username: 'keramiks',
+    name: 'keramiks',
+    description:
+      'Welcome to Keramiks, your online haven for exquisite handmade ceramics that blend artistry and functionality! At Keramiks, we are passionate about the beauty and craftsmanship of handmade ceramics, and we take great pride in curating a collection that will add a touch of elegance to your home.',
+    websiteUrl: 'www.keramiks.com',
+    location: 'Vienna',
+    imageUrl: '/images/keramiks.jpg',
+  },
+  {
+    id: 4,
+    username: 'thesoapcompany',
+    name: 'The Soap Company',
+    description:
+      'Welcome to The Soap Company, your ultimate online destination for luxurious, handcrafted soaps that transform your daily bathing ritual into a pampering experience! At The Soap Company, we are dedicated to crafting exceptional soaps that are not only gentle on your skin but also a feast for the senses. ',
+    websiteUrl: 'www.thesoapcompany.com',
+    location: 'Vienna',
+    imageUrl: '/images/thesoapcompany.jpg',
   },
 ];
 
