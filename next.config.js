917c5782034281f58ff8fe35edc6d0fe202c918c
cheorodio <cheorodio@gmail.com>
2023-07-01 10:37:54 +0200

    Allow cloudinary domain

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withPWA = require('next-pwa')({});
 
 const nextConfig = withPWA({
   dest: 'public',
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
   experimental: {
     typedRoutes: true,
     serverActions: true,
