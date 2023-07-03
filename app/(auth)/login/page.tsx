3a60d7f2c6ef14158c1d6795402335631efaf45b
cheorodio <cheorodio@gmail.com>
2023-07-03 22:43:51 +0200

    Change from login page from JS to typescript, products page from typescript to JS

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,9 +3,9 @@ import { redirect } from 'next/navigation';
 import { getValidSessionByToken } from '../../../database/sessions';
 import LoginForm from './Login';
 
-// type Props = { searchParams: { returnTo?: string | string[] } };
+type Props = { searchParams: { returnTo?: string } };
 
-export default async function LoginPage(searchParams) {
+export default async function LoginPage({ searchParams }: Props) {
   // redirect if the user is logged in
   // 1. check if the sessionToken cookie exists
   const sessionTokenCookie = cookies().get('sessionToken');
