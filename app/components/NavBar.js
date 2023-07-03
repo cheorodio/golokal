eb641e968243645dcd8fc444fe8b3631ca20d129
cheorodio <cheorodio@gmail.com>
2023-07-03 22:58:34 +0200

    Change to .js

---
+++ app/components/NavBar.js
@@ -0,0 +1,37 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import { CgClose } from 'react-icons/cg';
+import { RxHamburgerMenu } from 'react-icons/rx';
+import styles from '../styles/Navbar.module.scss';
+
+export const links = [
+  { id: 1, title: 'Home', link: '/' },
+  { id: 2, title: 'Shops', link: '/shops' },
+  { id: 3, title: 'About', link: '/about' },
+  { id: 4, title: 'Blog', link: '/blog' },
+];
+
+export default function NavBar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen((prev) => !prev);
+
+  return (
+    <>
+      <div className={`${styles.nav} ${menuOpen ? styles[`navOpen`] : {}}`}>
+        <ul className={styles.navLinks}>
+          {links.map(({ id, title, link }) => (
+            <Link href={link} key={`key-${id}`}>
+              <li>{title}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+
+      <button onClick={toggleMenu} className={styles.hamburgerIcon}>
+        {!menuOpen ? <RxHamburgerMenu /> : <CgClose />}
+      </button>
+    </>
+  );
+}
