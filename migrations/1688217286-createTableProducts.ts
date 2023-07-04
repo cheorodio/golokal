e1f3d424f12f58c4e6b7f080ac8df0231d5fd3b1
cheorodio <cheorodio@gmail.com>
2023-07-04 11:57:32 +0200

    Change all columns mandatory

--- migrations/1688217286-createTableProducts.ts
+++ migrations/1688217286-createTableProducts.ts
@@ -2,24 +2,24 @@ import { Sql } from 'postgres';
 
 export type Product = {
   id: number;
-  userId: number | null;
-  shopId: number | null;
-  name: string | null;
-  category: string | null;
-  description: string | null;
-  imageUrl: string | null;
+  userId: number;
+  shopId: number;
+  name: string;
+  category: string;
+  description: string;
+  imageUrl: string;
 };
 
 export async function up(sql: Sql) {
   await sql`
     CREATE TABLE products (
       id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-      user_id integer REFERENCES users (id),
-      shop_id integer REFERENCES shops (id),
-      name varchar(30),
-      category varchar(30),
-      description varchar(500),
-      image_url varchar(500)
+      user_id integer REFERENCES users (id) ON DELETE CASCADE,
+      shop_id integer REFERENCES shops (id) ON DELETE CASCADE,
+      name varchar(30) NOT NULL,
+      category varchar(30) NOT NULL,
+      description varchar(500) NOT NULL,
+      image_url varchar(500) NOT NULL
     )
   `;
 }
