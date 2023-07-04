3c3e4fe94dd51633cd04352d283611e6f356cdc2
cheorodio <cheorodio@gmail.com>
2023-07-04 11:57:43 +0200

    Change all columns mandatory

--- migrations/1688217209-createTableShops.ts
+++ migrations/1688217209-createTableShops.ts
@@ -2,11 +2,11 @@ import { Sql } from 'postgres';
 
 export type Shop = {
   id: number;
-  name: string | null;
-  description: string | null;
-  websiteUrl: string | null;
-  location: string | null;
-  imageUrl: string | null;
+  name: string;
+  description: string;
+  websiteUrl: string;
+  location: string;
+  imageUrl: string;
   userId: number | null;
 };
 
@@ -14,12 +14,12 @@ export async function up(sql: Sql) {
   await sql`
     CREATE TABLE shops (
       id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-      name varchar(30),
-      description varchar(500),
-      website_url varchar(80),
-      location varchar(40),
-      image_url varchar(500),
-      user_id integer REFERENCES users (id)
+      name varchar(30) NOT NULL,
+      description varchar(500) NOT NULL,
+      website_url varchar(80) NOT NULL,
+      location varchar(40) NOT NULL,
+      image_url varchar(500) NOT NULL,
+      user_id integer REFERENCES users (id) ON DELETE CASCADE
     )
   `;
 }
