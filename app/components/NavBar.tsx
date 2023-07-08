e35e884108d6fc653db58a14d9c207dcf65eee68
cheorodio <cheorodio@gmail.com>
2023-07-08 13:50:08 +0200

    Change file to typescript

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import { BiSolidChevronDown } from 'react-icons/bi';
 import { CgClose } from 'react-icons/cg';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import styles from '../styles/Navbar.module.scss';
@@ -21,11 +22,39 @@ export default function NavBar() {
     <>
       <div className={`${styles.nav} ${menuOpen ? styles[`navOpen`] : {}}`}>
         <ul className={styles.navLinks}>
-          {links.map(({ id, title, link }) => (
+          {/* {links.map(({ id, title, link }) => (
             <Link href={link} key={`key-${id}`}>
               <li>{title}</li>
             </Link>
-          ))}
+          ))} */}
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+
+          <li className={styles.discoverDropdown}>
+            <div className={styles.discoverDropdownButton}>
+              <p>Discover</p>
+              <BiSolidChevronDown />
+            </div>
+
+            <div className={styles.discoverDropdownOptions}>
+              <ul>
+                <li>
+                  <Link href="/shops">Shops</Link>
+                </li>
+                <li>
+                  <Link href="/products">Products</Link>
+                </li>
+              </ul>
+            </div>
+          </li>
+
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
         </ul>
       </div>
 
