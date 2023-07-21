ebb15835bddbcdc840cc28ac96f9311398627193
cheorodio <cheorodio@gmail.com>
2023-07-21 17:35:44 +0200

    Add and style a section in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import connect from '../public/images/icons/connect.png';
 import discover from '../public/images/icons/discover.png';
 import explore from '../public/images/icons/explore.png';
+import keramiks from '../public/images/keramiks.jpg';
 import about from '../public/images/vida.jpg';
 import styles from './styles/homepage.module.scss';
 
@@ -16,7 +17,7 @@ export default function Home() {
         <h1 className={domine.className}>
           Connecting Communities, Supporting Small Businesses
         </h1>
-        <p>Discover local vendors and hundreds of handmade products</p>
+        {/* <p>Discover local vendors and hundreds of handmade products</p> */}
         <Link href="/shops" className={styles.heroLink}>
           explore now
         </Link>
@@ -64,109 +65,34 @@ export default function Home() {
           </div>
         </div>
       </section>
-      {/*
-      <section className={styles.aboutSection}>
-        <h2>Who are we?</h2>
-        <p className={styles.introduction}>
-          GoLokal is your ultimate destination for local and handmade products.
-          We are passionate about supporting artisans, craftsmen, and local
-          businesses, and our platform serves as a virtual marketplace to
-          showcase their unique creations.
-        </p>
-        <div className={styles.highlights}>
-          <div>
-            <h6>01</h6>
-            <h4>Explore</h4>
-            <Image
-              className={styles.iconHidden}
-              src={explore}
-              alt="explore icon"
-            />
-            <p>
-              Explore a thoughtfully curated collection of local and handmade
-              products, handpicked for their exceptional quality, uniqueness,
-              and craftsmanship.
-            </p>
-          </div>
-          <div>
-            <h6>02</h6>
-            <h4>Discover</h4>
-            <Image
-              className={styles.iconHidden}
-              src={discover}
-              alt="discover icon"
-            />
-            <p>
-              Connect with a vibrant community of like-minded individuals who
-              share your love for locally made goods. Engage in discussions,
-              attend events, and collaborate with fellow enthusiasts and
-              artisans.
-            </p>
-          </div>
-          <div>
-            <h6>03</h6>
-            <h4>Connect</h4>
-            <Image
-              className={styles.iconHidden}
-              src={connect}
-              alt="connect icon"
-            />
-            <p>
-              Connect with a vibrant community of like-minded individuals who
-              share your love for locally made goods. Engage in discussions,
-              attend events, and collaborate with fellow enthusiasts and
-              artisans.
-            </p>
-          </div>
-        </div>
-      </section> */}
 
-      {/* <section className={styles.discoverSection}>
-        <div className={styles.imageContainer}>
+      <section className={styles.discoverSection}>
+        <div className={styles.imageSection}>
           <Image
-            className={styles.discoverImage}
-            src={discoverHeader}
-            alt="Image of a girl wearing handmade polymer clay earrings"
+            className={styles.vendorImage}
+            height={400}
+            width={400}
+            src={keramiks}
+            alt="vendor"
           />
         </div>
-        <div className={styles.textContainer}>
-          <h2>Discover local vendors with over 1000 handmade products</h2>
-          <p>
+
+        <div className={styles.infoSection}>
+          <h2 className={`${domine.className} ${styles.discoverTitle}`}>
+            Discover local vendors with over 1000 handmade products
+          </h2>
+          <p className={styles.discoverInfo}>
             By supporting local and handmade products, you become an integral
             part of a movement that promotes community empowerment, fosters
             creativity, and fuels economic growth at a grassroots level.
           </p>
-          <Link href="/" className={styles.discoverButton}>
-            explore now
-          </Link>
-        </div>
-      </section> */}
-      {/*
-      <section className={styles.statistics}>
-        <Image
-          className={styles.potteryImage}
-          src={statistic}
-          alt="image of a hand making pottery"
-        />
-        <div className={styles.information}>
-          <div>
-            <h3>1000</h3>
-            <p>handmade products</p>
-          </div>
-          <div>
-            <h3>20</h3>
-            <p>categories to choose from</p>
-          </div>
-          <div>
-            <h3>15</h3>
-            <p>registered vendors</p>
-          </div>
-          <div>
-            <h3>300</h3>
-            <p>registered users</p>
+          <div className={styles.linkContainer}>
+            <Link href="/shops" className={styles.exploreLink}>
+              Explore now
+            </Link>
           </div>
         </div>
-      </section> */}
+      </section>
     </main>
   );
 }
